0ff0fd8711a13c6d339c29fe603c4938a0385af2
Susan Dreher <sdreher@columbia.edu>
2016-08-01 15:55:26 -0400

    npm3: referencing relative node_modules directory is a no no

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ require('!file?name=[name].[ext]!../static/index.html');
 require('./static.js');
 
 // load and apply css
-require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
-require('../node_modules/bootstrap-arrow-buttons/dist/' +
-        'css/bootstrap-arrow-buttons.css');
+require('!style!css!bootstrap/dist/css/bootstrap.min.css');
+require('!style!css!bootstrap-arrow-buttons/dist/css/' +
+        'bootstrap-arrow-buttons.css');
 require('../static/css/common.css');
 require('../static/css/infographic.css');
 
