eb768e21ee73e574cbc24b4ab177e850d1dba84f
zarina <zarina@columbia.edu>
2017-01-11 15:14:56 -0500

    Added logo paths definition

--- src/static.js
+++ src/static.js
@@ -1,4 +1,5 @@
-require('!file?name=[name].[ext]!../static/img/ctl-logo.png');
+require('!file?name=[name].[ext]!../static/img/logo-ctl.png');
+require('!file?name=[name].[ext]!../static/img/logo-cdm.png');
 require('!file?name=[name].[ext]!../static/img/dresser_candies_t.png');
 require('!file?name=[name].[ext]!../static/img/dresser_familyphoto_t.png');
 require('!file?name=[name].[ext]!../static/img/dresser_dentures_t.png');
